bafedf8df1d045ae44ebe0dfcf476c5adbf4d2a1
Ace <andreasc002@gmail.com>
2023-08-07 23:47:08 +0700

    test

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
+	password := "aakbjkgbejanfn213j5n13n45l1n5"
+
+	fmt.Println("Password is: ", password)
+
 	a := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	b := a[1:4]
 	fmt.Println("a:", a)
