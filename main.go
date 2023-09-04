9d420f29da73d81b0d0af7d66314dfcabc6ad9f0
Ace <andreasc002@gmail.com>
2023-09-04 15:54:49 +0700

    test commenting code

--- main.go
+++ main.go
@@ -6,26 +6,26 @@ import (
 )
 
 func main() {
-	password := "aakbjkgbejanfn213j5n13n45l1n5"
+	// password := "aakbjkgbejanfn213j5n13n45l1n5"
 
-	fmt.Println("Password is: ", password)
+	// fmt.Println("Password is: ", password)
 
-	a := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	b := a[1:4]
-	fmt.Println("a:", a)
-	fmt.Println("b:", b)
+	// a := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// b := a[1:4]
+	// fmt.Println("a:", a)
+	// fmt.Println("b:", b)
 
-	// Works fine even though c is indexing past the end of b.
-	c := b[4:7]
-	fmt.Println("c:", c)
+	// // Works fine even though c is indexing past the end of b.
+	// c := b[4:7]
+	// fmt.Println("c:", c)
 
-	// This fails with panic: runtime error: index out of range [4] with length 3
-	d := b[4]
-	fmt.Println("d:", d)
+	// // This fails with panic: runtime error: index out of range [4] with length 3
+	// d := b[4]
+	// fmt.Println("d:", d)
 
-	if true {
-		fmt.Println("WOHO")
-	}
+	// if true {
+	// 	fmt.Println("WOHO")
+	// }
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %d!", UncoverFunc()+CoverFunc())
